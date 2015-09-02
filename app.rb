f83c63cb09630e75d9c087d5992ce28c4a6cab00
Sam Rae <samuelrae@gmail.com>
2015-09-02 17:42:58 +0100

    put in form with input box that returns the Pig Latin word for the word entered

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require_relative './models/pig_latin'
 
-get '/:phrase'  do
-  "#{PigLatin.translate params[:phrase]}"
-end
+  get '/' do 
+    @title = 'home'
+    erb :home
+  end
+
+  post '/translate' do
+    redirect to "/#{params[:word]}"
+  end
+
+  get '/:phrase'  do
+    "#{PigLatin.translate params[:phrase]}"
+  end
--- models/pig_latin.rb
+++ models/pig_latin.rb
@@ -2,7 +2,7 @@ class PigLatin
 
   def self.translate phrase
     new_phrase =''
-    case phrase.start_with?('a','e','i','o','u')
+    case phrase.downcase.start_with?('a','e','i','o','u')
     when true then new_phrase = "#{phrase}ay"
     else new_phrase = phrase[1..phrase.length].concat phrase[0].concat "ay"
     end
